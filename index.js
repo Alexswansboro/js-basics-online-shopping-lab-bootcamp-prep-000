e901719c1c330e96b97649931e13b66ffe179c33
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-06-13 16:28:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function placeOrder(cardNumber) {
  } 
  else (cardNumber ===`${cardNumber}`) 
   
- return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+ return `Your total cost is ${total()},which will be charged to the card ${cardNumber}.`
   cart = []
 }
 
