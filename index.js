1d37032683fc08fea79917e1d8e3e7dce5f80424
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-06-13 16:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,11 +56,11 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
  if (!cardNumber) {
    return "Sorry, we don't have a credit card on file for you."
-   cart = []
+  
  } 
- else (cardNumber ===`${cardNumber}`) 
+ else (cardNumber) 
   
  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
- 
+  cart = []
 }
  
